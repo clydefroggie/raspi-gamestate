fa0a6da05479c08fdbf07bd534ceb7d0ee876ea3
clydefroggie <clydefroggie@hotmail.com>
2020-07-17 15:36:18 +0200

    Added classes for collision - not working yet-

--- begin_pygame.py
+++ begin_pygame.py
@@ -11,16 +11,32 @@ bkg = pygame.image.load('sprites/bkg_1.png')
 
 clock = pygame.time.Clock()
 
-class Pusher (object):
-    def __init__(self, x_pos, y_pos, width, height, bot=False):
-        self.x = x_pos
-        self.y = y_pos
+class Pusher (pygame.sprite.Sprite):
+    def __init__(self, width, height, bot=False):
+        super().__init__()
+        
         self.width, self.height = width, height
         
         self.vel = 5  
         
-    def draw(self, win):
-        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, self.width, self.height))                
+        self.image = pygame.Surface([self.width, self.height])
+        BLACK = (0,0,0)
+        self.image.fill(BLACK)
+        
+        pygame.draw.rect(self.image, BLACK,[0, 0, self.width, self.height])
+        self.rect = self.image.get_rect()
+        
+        self.x_pos = self.rect[0]
+        self.y_pos = self.rect[1]
+#    def get_x_pos(self):
+#        return self.rect[0]
+#    
+#    def get_y_pos(self):
+#        return self.rect[1]
+#    def draw(self, win):
+#        pass
+#        pygame.draw.rect(self.img, (255,255,0),[self.x, self.y, self.width, self.height])
+#        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y, self.width, self.height))                
 
 class Puck (object):
     #todo: check how to do inheritence
@@ -29,7 +45,7 @@ class Puck (object):
         self.y_pos = y_pos
         self.width, self.height = width, height        
         
-        self.vel = 10
+        self.vel = 6
         self.x_dir, self.y_dir = 1, 1
         
     def update_x_dir(self):
@@ -49,13 +65,21 @@ class Puck (object):
         
 def redrawGameWindow():   
     win.blit(bkg, (0, 0))
-    player_one.draw(win)
-    puck.draw(win)
+#    player_one.draw(win)
+#    puck.draw(win)
+    pygame.display.flip()
+    list_all_sprites.draw(win)
     pygame.display.update()
 
 #mainloop
-player_one = Pusher(0, WIN_HEIGHT-64, 20, 64)
+
+player_one = Pusher(20, 64)
+player_one.rect = (100, 100)
+
 puck = Puck(150, 150, 5, 5)
+
+list_all_sprites = pygame.sprite.Group()
+list_all_sprites.add(player_one)
 
 run = True
 while run:
@@ -64,29 +88,30 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+            
+#    x_pos, y_pos = player_one.rect
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        if (player_one.y - player_one.vel) < 0:
-            player_one.y = 0 
+        if (player_one.y_pos - player_one.vel) < 0:
+            player_one.y_pos = 0 
         else:
-            player_one.y -= player_one.vel
-    if keys[pygame.K_DOWN]:
-        if (player_one.y + player_one.height + player_one.vel) > WIN_HEIGHT:
-            player_one.y = WIN_HEIGHT - player_one.height
-        else:
-            player_one.y += player_one.vel
-
-    if (puck.x_pos + puck.width) > WIN_WIDTH:
-        puck.update_x_dir()
-    if puck.y_pos < 0 or (puck.y_pos + puck.height) > WIN_HEIGHT:
-        puck.update_y_dir()
- 
-    if puck.x_pos <= (player_one.x + player_one.width):
-        puck.update_x_dir()
-        if not (puck.y_pos >= (player_one.y - puck.height) and puck.y_pos <= (player_one.y + player_one.height + puck.height)):
-            print("NOK")
-    
+            player_one.y_pos -= player_one.vel
+#    if keys[pygame.K_DOWN]:
+#        if (player_one.rect.y + player_one.height + player_one.vel) > WIN_HEIGHT:
+#            player_one.rect.y = WIN_HEIGHT - player_one.height
+#        else:
+#            player_one.rect.y += player_one.vel
+#
+#    if (puck.x_pos + puck.width) > WIN_WIDTH:
+#        puck.update_x_dir()
+#    if puck.y_pos < 0 or (puck.y_pos + puck.height) > WIN_HEIGHT:
+#        puck.update_y_dir()
+# 
+#    if puck.x_pos <= (player_one.x + player_one.width):
+#        puck.update_x_dir()
+#        if not (puck.y_pos >= (player_one.y - puck.height) and puck.y_pos <= (player_one.y + player_one.height + puck.height)):
+#            print("NOK")
+#    
     redrawGameWindow()
     
 pygame.quit()
